ba729ff5223d22471ca1a6803edd36388051bebc
MIN TAE YEONG <dndbxhd10@naver.com>
2024-09-18 15:27:54 +0900

    props의 룰

--- 04_react_basic/src/120_props_rules/start/components/Hello.jsx
+++ 04_react_basic/src/120_props_rules/start/components/Hello.jsx
@@ -1,4 +1,8 @@
 const Hello = (props) => {
+  // propsの属性の値は変えることはできない
+  // props.name = "awdsadasdsw"
+  // 
+  console.log(Reflect.getOwnPropertyDescriptor(props, "name"));
   return (
     <div>
       <h3>Hello {props.name}</h3>
