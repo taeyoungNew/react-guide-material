6e3f482a5d1ec05f2b407767bb4f099da385d933
MIN TAE YEONG <dndbxhd10@naver.com>
2024-09-18 09:21:19 +0900

    end

--- 04_react_basic/src/085_expr_and_state/start/components/Child.jsx
+++ 04_react_basic/src/085_expr_and_state/start/components/Child.jsx
@@ -1,14 +1,20 @@
 /* POINT 式と文
 式：何らかの値を返すもの（変数に代入できるもの）
 文：変数宣言、for文、if文、switch文やセミコロンで区切るもの。
+jsx内では式は使用可能しかし文は使用部可能となる
 */
 
 import "./Child.css";
 
 const Child = () => {
+  const hello = () => "hello"
+  const fn = () => "hello"
+  const a = hello();
   return (
     <div className="component">
       <h3>式と文</h3>
+      {fn()}
+      {a}
     </div>
   );
 };
