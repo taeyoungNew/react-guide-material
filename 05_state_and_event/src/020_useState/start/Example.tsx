f5c3a39d95e545f8a312ca6a78e5f042b869e091
MIN TAE YEONG <dndbxhd10@naver.com>
2024-09-19 13:15:56 +0900

    a

--- 05_state_and_event/src/020_useState/start/Example.tsx
+++ 05_state_and_event/src/020_useState/start/Example.tsx
@@ -7,7 +7,7 @@ const Example = () => {
   // 0번째는 값이 저장되는 변수
   // 1번째는 값을 저장하는 함수
   // 제네릭타입으로 number타입과 string타입을 받을 수 있게하자
-  let [val, setVal] = useState<number | string>(0);
+  let [val, setValue] = useState<number | string>(0);
   
   
   return (
@@ -15,7 +15,7 @@ const Example = () => {
       <input 
         type="text"
         onChange={(event) => {
-          const setFn = setVal
+          const setFn = setValue
           setFn(event.target.value)
         }} 
       /> = {val}
