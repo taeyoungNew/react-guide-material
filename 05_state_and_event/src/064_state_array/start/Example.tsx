ab91956ee5d617aaf1f9c002b160a123f6e65d0f
MIN TAE YEONG <dndbxhd10@naver.com>
2024-09-20 09:25:34 +0900

    use state with array

--- 05_state_and_event/src/064_state_array/start/Example.tsx
+++ 05_state_and_event/src/064_state_array/start/Example.tsx
@@ -2,12 +2,24 @@ import { useState } from "react";
 
 const Example = () => {
   const numArray = [1, 2, 3, 4, 5];
-  const [ array, shuffleArr ] = useState(numArray)
+  const [ array, setArray ] = useState(numArray)
+  let newNumArr = [...array] // 배열을 완전 복사
+
   const letShuffle = () => {
-    const result = array.map((el) => ({ sort: Math.random(), value: el}))
-        .sort((a, b) => a.sort - b.sort)
-        .map((el) => el.value)
-    shuffleArr(result)
+    console.log("믹");
+    
+    let lastNum = newNumArr.pop()
+    if(typeof lastNum === "number") {
+      console.log("if문", lastNum);
+      console.log("newNumArr = ", newNumArr);
+      
+      newNumArr.unshift(lastNum)
+    }
+    // newNumArr.shift(lastNum)
+    // const result = array.map((el) => ({ sort: Math.random(), value: el}))
+    //     .sort((a, b) => a.sort - b.sort)
+    //     .map((el) => el.value)
+    setArray(newNumArr)
   }
   
   return (
